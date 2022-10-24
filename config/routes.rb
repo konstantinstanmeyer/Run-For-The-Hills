9beb9090dc0892e42d4e9e1697acfe4c49e54d9e
Shelby Lee <slee337@uw.edu>
2022-10-24 11:35:31 -0600

    added custom login routes and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to:'sessions#delete' 
+  get '/authorized_user', to: 'users#show'
 end
