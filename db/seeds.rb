a74eb28ce359c650c856d6bb2bbed0dfb6d0d4f2
Shelby Lee <slee337@uw.edu>
2022-10-25 09:57:52 -0600

    some seeds for ya

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,47 @@
 
 puts "seeding"
 
+u1 = User.create(
+    email: 'a@gmail.com',
+    first_name: 'Billy',
+    last_name: 'Bob',
+    password_digest: 'weasdfji'
+)
+
+u2 = User.create(
+    email: 'b@gmail.com',
+    first_name: 'Joe',
+    last_name: 'Jobs',
+    password_digest: 'wefsdv'
+)
+
+u3 = User.create(
+    email: 'c@gmail.com',
+    first_name: 'Bertha',
+    last_name: 'Branson',
+    password_digest: 'asdbrt'
+)
+
+u4 = User.create(
+    email: 'd@gmail.com',
+    first_name: 'Wendy',
+    last_name: 'Baconator',
+    password_digest: 'sdfasfaew'
+)
+
+p1 = Profile.create(
+    profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
+    photo_png: 'nill',
+    beard_length: 0,
+    moonshine_abv_level: 21.3,
+    rodeo_buckles: 0,
+    truck_brand: 'DUI',
+    mode_of_tobacco: 'chew',
+    pontoon_boat?: false,
+    security_goat?: true,
+    user_id: u1.id
+)
+
 p2 = Profile.create(
     profile_picture: 'https://sexyscience1.files.wordpress.com/2013/11/worst-online-dating-profile-photo-8.jpg',
     photo_png: 'nill',
@@ -18,7 +59,7 @@ p2 = Profile.create(
     mode_of_tobacco: 'chew',
     pontoon_boat?: false,
     security_goat?: false,
-    user_id: 2
+    user_id: u2.id
 )
 
 p3 = Profile.create(
@@ -31,7 +72,7 @@ p3 = Profile.create(
     mode_of_tobacco: 'roach',
     pontoon_boat?: false,
     security_goat?: true,
-    user_id: 3
+    user_id: u3.id
 )
 
 
@@ -45,7 +86,7 @@ p4 = Profile.create(
     mode_of_tobacco: 'all',
     pontoon_boat?: true,
     security_goat?: true,
-    user_id: 4
+    user_id: u4.id
 )
 
 puts "done seeding"
