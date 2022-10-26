dd407540902352d2dee8847a38e293c27699a87a
Shelby Lee <slee337@uw.edu>
2022-10-26 11:56:15 -0600

    pushing to go wtih the front end push for sign up page and eidt profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,11 +13,17 @@ class ProfilesController < ApplicationController
         profile.update!(profile_params)
         render json: profile, status: :accepted
     end
+
+    def create
+        profile = Profile.create!(profile_params)
+        render json: profile, status: :created
+    end
+
     
     private 
 
     def profile_params
-        params.permit(:profile_picture, :photo_png, :beard_length, :moonshine_abv_level, :rodeo_buckles, :truck_brand, :mode_of_tobacco, :pontoon_boat?, :security_goat?)
+        params.permit(:profile_picture, :photo_png, :beard_length, :moonshine_abv_level, :rodeo_buckles, :truck_brand, :mode_of_tobacco, :pontoon_boat?, :security_goat?, :user_id)
     end
 
 
